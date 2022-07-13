c339f170b7d606f14089b23b1ef8db778b897f46
Islam Tarek <islamtarek0550@gmail.com>
2022-07-13 08:20:30 +0200

    Add Special function I/O register and its bits to core file

--- Atmega32/core.h
+++ Atmega32/core.h
@@ -3,3 +3,18 @@
 /*                                              SW Module   : Atmega32 core                                                                 */
 /*                                              Last Update : 13/7/2022                                                                     */
 /********************************************************************************************************************************************/
+#ifndef ATMEGA32_CORE_H_
+#define ATMEGA32_CORE_H_
+
+/* Special function input/ output register and its pins */
+#define SFIOR                                       *((volatile uint8_t *)(0x50))
+
+#define ADTS2           7 
+#define ADTS1           6
+#define ADTS0           5
+#define ACME            3
+#define PUD             2
+#define PSR2            1
+#define PSR10           0
+
+#endif
