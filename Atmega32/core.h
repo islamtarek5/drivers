88057a6dc32973dd85742d6104280ef84b0f97b9
Islam Tarek <islamtarek0550@gmail.com>
2022-07-13 08:21:42 +0200

    Add GPIO Registers to core file

--- Atmega32/core.h
+++ Atmega32/core.h
@@ -17,4 +17,24 @@
 #define PSR2            1
 #define PSR10           0
 
+/* GPIO Registers */
+
+#define GPIO_A_BASE_ADDRESS                         0x39
+#define GPIO_B_BASE_ADDRESS                         0x36
+#define GPIO_C_BASE_ADDRESS                         0x33
+#define GPIO_D_BASE_ADDRESS                         0x30
+typedef struct 
+{
+    volatile uint8_t PIN;
+    volatile uint8_t DDR;
+    volatile uint8_t PORT;
+}gpio_reg_t;
+
+#define GPIO_A_REG                                        (gpio_reg_t *)(GPIO_A_BASE_ADDRESS)          
+#define GPIO_B_REG                                        (gpio_reg_t *)(GPIO_B_BASE_ADDRESS)
+#define GPIO_C_REG                                        (gpio_reg_t *)(GPIO_C_BASE_ADDRESS)
+#define GPIO_D_REG                                        (gpio_reg_t *)(GPIO_D_BASE_ADDRESS)
+
+
+
 #endif
